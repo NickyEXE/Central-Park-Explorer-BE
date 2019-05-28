8c3103fc5a7d468a30c99cba3e6c1238936eaeda
NickyEXE <dovern42@gmail.com>
2019-05-28 16:38:23 -0400

    updated serializer to give locations nearest places

--- app/serializers/location_serializer.rb
+++ app/serializers/location_serializer.rb
@@ -1,9 +1,15 @@
 class LocationSerializer < ActiveModel::Serializer
-  attributes :name, :description, :id, :landmarks, :facts, :locimages, :tags, :center
+  attributes :name, :description, :id, :landmarks, :facts, :locimages, :tags, :center, :nearby_places
 
 
   def tags
     tags = LocationTag.where(location_id: self.object.id).map{|tag| tag.tag_id}.uniq
     Tag.where(id: tags)
   end
+
+  def nearby_places
+    center = self.object.center
+    Location.nearest_places(center[:latitude], center[:longitude])
+  end
+
 end
